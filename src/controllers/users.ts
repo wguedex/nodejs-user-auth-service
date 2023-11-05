cd8076dd7c3b4d44b436e67ca85644861c00b315
Wilmer Guedez <w.guedex@gmail.com>
2023-11-04 21:16:49 -0500

    feat: add getUserById endpoint
    - Added a new endpoint to retrieve a user by their ID.
      The 'getUserById' controller handles GET requests to '/api/users/:id'.

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,11 +1,11 @@
 // Import the 'User' model from '../models/user'.
-import User from '../models/user';
+import User from "../models/user";
 
 // Import the 'request' and 'response' objects from the 'express' library.
 import { request, response } from "express";
 
 // Define an asynchronous function 'getUsers' to handle user retrieval.
-export const getUsers = async (req = request, res = response) => {
+const getUsers = async (req = request, res = response) => {
   try {
     // Extract query parameters 'limit' and 'from' from the request.
     const { limit = 5, from = 0 } = req.query;
@@ -19,20 +19,37 @@ export const getUsers = async (req = request, res = response) => {
       User.countDocuments(query),
 
       // Find users that match the query, with pagination using 'limit' and 'from'.
-      User.find(query)
-        .skip(Number(from))
-        .limit(Number(limit))
+      User.find(query).skip(Number(from)).limit(Number(limit)),
     ]);
 
     // Respond with JSON containing the total count and the retrieved users.
     res.json({
       total,
-      users
-    });    
- 
+      users,
+    });
   } catch (error) {
     // Handle errors by logging and sending a 500 Internal Server Error response.
-    console.error('Error getting users:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
+    console.error("Error getting users:", error);
+    res.status(500).json({ error: "Internal Server Error" });
   }
+};
+
+const getUserById = async (req = request, res = response) => {
+  try {
+    const userId = req.params.id;
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ error: 'Error getting user by ID' });
+  }
+};
+
+module.exports = {
+  getUsers,
+  getUserById,
 };
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -2,7 +2,7 @@
 const { Router } = require('express'); 
 
 // Import the 'getUsers' function from the 'users' controller.
-const { getUsers } = require('../controllers/users');
+const { getUsers, getUserById } = require('../controllers/users');
 
 // Create a new instance of the 'Router' class.
 const router = Router();
@@ -10,6 +10,10 @@ const router = Router();
 // Define a route to handle GET requests to the root path ('/').
 // It uses the 'getUsers' function from the controller to handle the request.
 router.get('/', getUsers);
+
+// Define a route to handle GET requests to '/api/users/:id'.
+// It uses the 'getUserById' function from the controller to handle the request.
+router.get('/:id', getUserById);
 
 // Export the 'router' instance to make it available for use in other parts of the application.
 module.exports = router;
