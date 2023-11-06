2b45070a1be73b89c06da23d7afa03ca6be904cc
Wilmer Guedez <w.guedex@gmail.com>
2023-11-05 22:27:08 -0500

    feat: Added role validation middleware
    Implemented role validation middleware for verifying user roles. The middleware includes 'isAdminRole' and 'hasRole' functions. 'isAdminRole' checks if the user is an administrator, while 'hasRole' checks if the user has one of the specified roles.
    
    Changes:
    - Added 'validate-roles.ts' for role validation middleware.
    - Implemented 'isAdminRole' and 'hasRole' middleware functions for role validation.

---
+++ src/middlewares/validate-roles.ts
@@ -0,0 +1,45 @@
+const { response } = require("express");
+
+import { Request, Response, NextFunction } from "express";
+
+const isAdminRole = (req: Request, res: Response, next: NextFunction) => {
+  const user = req.params;
+
+  if (!user) {
+    return res.status(500).json({
+      msg: "Trying to verify the role without validating the token first",
+    });
+  }
+
+  const { role, name } = req.params;
+
+  if (role !== "ADMIN_ROLE") {
+    return res.status(401).json({
+      msg: `${name} is not an administrator - Cannot do this`,
+    });
+  }
+
+  next();
+};
+
+const hasRole = (...roles: string[]) => {
+  return (req: Request, res: Response = response, next: NextFunction) => {
+    const user = req.params;
+
+    if (!req.user) {
+      return res.status(500).json({
+        msg: "Trying to verify the role without validating the token first",
+      });
+    }
+
+    if (!roles.includes(user.role)) {
+      return res.status(401).json({
+        msg: `The service requires one of these roles: ${roles.join(", ")}`,
+      });
+    }
+
+    next();
+  };
+};
+
+export { isAdminRole, hasRole };
