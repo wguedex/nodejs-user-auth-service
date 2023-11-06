01adc21edf35803e7143f3f48b4d8d143b9aeb61
Wilmer Guedez <w.guedex@gmail.com>
2023-11-05 22:22:06 -0500

    feat: Added middleware for validating request fields
    Implemented a middleware function 'validateFields' for checking request field validations using 'express-validator'. The function checks for validation errors and returns a 400 (Bad Request) response with error details if any errors are found.
    
    Changes:
    - Added 'validate-fields.ts' for field validation middleware.
    - Implemented 'validateFields' middleware function for request validation.

---
+++ src/middlewares/validate-fields.ts
@@ -0,0 +1,22 @@
+// Import necessary modules and types from Express and express-validator.
+import { validationResult } from 'express-validator';
+import { Request, Response, NextFunction } from "express";
+
+// Define a middleware function called 'validateFields' that takes
+// Request, Response, and NextFunction as arguments.
+const validateFields = (req: Request, res: Response, next: NextFunction) => {
+    // Use express-validator's 'validationResult' function to check for validation errors.
+    const errors = validationResult(req);
+
+    // If there are validation errors, send a response with a 400 (Bad Request) status
+    // and an error message in JSON format.
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ error: 'Bad Request' });
+    }
+
+    // If there are no validation errors, proceed to the next middleware.
+    next();
+};
+
+// Export the 'validateFields' function for use in other parts of the application.
+module.exports = validateFields;
